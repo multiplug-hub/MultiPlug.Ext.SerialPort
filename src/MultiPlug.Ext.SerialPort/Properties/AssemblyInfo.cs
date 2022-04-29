1e4170e84014c19b3459f15f957cd78fcba43c5c
David Graham <hello@industry4.uk>
2022-04-29 10:01:00 +0100

    Release 1.0.0

---
+++ src/MultiPlug.Ext.SerialPort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MultiPlug.Ext.SerialPort")]
+[assembly: AssemblyDescription("Serial Port MultiPlug Extension")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MultiPlug Community")]
+[assembly: AssemblyProduct("Serial Port")]
+[assembly: AssemblyCopyright("MultiPlug Community 2022")]
+[assembly: AssemblyTrademark("MultiPlug")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cce80f60-522e-4e91-9e64-2da4bdf1dd94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
